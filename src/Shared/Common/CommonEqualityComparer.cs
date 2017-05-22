eaafe9e4431fa7e612a29cd4c33410df3a4bc510
lanymy <lanyanmiyu@qq.com>
2017-05-22 11:20:19 +0800

    添加单元测试项目

---
+++ src/Shared/Common/CommonEqualityComparer.cs
@@ -0,0 +1,61 @@
+﻿/********************************************************************
+
+时间: 2015年11月03日, AM 11:05:53
+
+作者: lanyanmiyu@qq.com
+
+描述: 通用比较器
+
+其它:     
+
+********************************************************************/
+
+
+
+using System;
+using System.Collections.Generic;
+
+
+namespace Lanymy.General.Extension.Common
+{
+
+
+    /// <summary>
+    /// 通用比较器
+    /// </summary>
+    /// <typeparam name="T">要比较的实体类型</typeparam>
+    /// <typeparam name="TValue">要比较实体中的具体属性类型</typeparam>
+    public class CommonEqualityComparer<T, TValue> : IEqualityComparer<T>
+    {
+
+        private Func<T, TValue> _KeySelector;
+
+        private IEqualityComparer<TValue> _Comparer;
+
+        public CommonEqualityComparer(Func<T, TValue> keySelector, IEqualityComparer<TValue> comparer)
+        {
+            this._KeySelector = keySelector;
+            this._Comparer = comparer;
+        }
+
+
+        public CommonEqualityComparer(Func<T, TValue> keySelector)
+            : this(keySelector, EqualityComparer<TValue>.Default)
+        { }
+
+        public bool Equals(T x, T y)
+        {
+            return _Comparer.Equals(_KeySelector(x), _KeySelector(y));
+        }
+
+
+        public int GetHashCode(T obj)
+        {
+            return _Comparer.GetHashCode(_KeySelector(obj));
+        }
+
+
+    }
+
+
+}
