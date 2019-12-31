9bbe6625d8cc91c6eaed6f408465340e4cf04263
﹎蓝言觅ぷ雨 <lanyanmiyu@qq.com>
2019-12-31 10:29:59 +0800

    RegisterDriverFullPathToEnvironmentVariables 注册 非托管 DLL (如:驱动文件) 文件夹目录 到 当前应用程序域进程的环境变量中

--- src/Commons/Lanymy.Common/DriversHelper.cs
+++ src/Commons/Lanymy.Common/DriversHelper.cs
@@ -1,18 +1,23 @@
-﻿#if NET48
-
+﻿
 
 using System;
 using System.IO;
-using System.Runtime.InteropServices;
 using Lanymy.Common.ConstKeys;
+
+//using System.Runtime.InteropServices;
+//using Lanymy.Common.ConstKeys;
 
 namespace Lanymy.Common
 {
+
     /// <summary>
     /// 驱动辅助类
     /// </summary>
     public class DriversHelper
     {
+
+
+#if NET48
 
         /// <summary>
         /// 挂载驱动文件 并 返回 句柄
@@ -38,10 +43,35 @@ namespace Lanymy.Common
 
         }
 
+#endif
+
+
+        /// <summary>
+        /// 注册 非托管 DLL (如:驱动文件) 文件夹目录 到 当前应用程序域进程的环境变量中
+        /// </summary>
+        /// <param name="driverFullPath">非托管 DLL 文件夹目录 支持同时注册多个</param>
+        public static void RegisterDriverFullPathToEnvironmentVariables(params string[] driverFullPath)
+        {
+
+            var target = EnvironmentVariableTarget.Process;
+
+            var pathValue = Environment.GetEnvironmentVariable(EnvironmentVariableKeys.PATH_KEY, target);
+
+            if (!pathValue.EndsWith(EnvironmentVariableKeys.ENVIRONMENT_VARIABLE_SEPARATOR))
+            {
+                pathValue += EnvironmentVariableKeys.ENVIRONMENT_VARIABLE_SEPARATOR;
+            }
+
+            pathValue += string.Join(EnvironmentVariableKeys.ENVIRONMENT_VARIABLE_SEPARATOR, driverFullPath);
+
+            Environment.SetEnvironmentVariable(EnvironmentVariableKeys.PATH_KEY, pathValue, target);
+
+        }
+
 
 
     }
+
 }
 
 
-#endif
