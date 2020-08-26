2f992a7aa0e2d18960501cc1981797b380e949ca
lanymy <lanyanmiyu@qq.com>
2020-08-26 17:55:23 +0800

    初始化单元测试项目

---
+++ src/UnitTests/Lanymy.Common.Instruments.IsolatedStoragesTests/LanymyIsolatedStorageTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lanymy.Common.Instruments;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lanymy.Common.Instruments.Tests
+{
+    [TestClass()]
+    public class LanymyIsolatedStorageTests
+    {
+        [TestMethod()]
+        public void SaveStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
