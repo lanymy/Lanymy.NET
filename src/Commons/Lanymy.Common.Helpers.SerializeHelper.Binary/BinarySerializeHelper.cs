a28ad027e47c759d7a5852238e248ba6c14a0568
lanymy <lanyanmiyu@qq.com>
2020-12-18 11:29:17 +0800

    Lanymy.Common.Helpers.SerializeHelper.Binary Lanymy.Common.Helpers.SerializeHelper.File

---
+++ src/Commons/Lanymy.Common.Helpers.SerializeHelper.Binary/BinarySerializeHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Lanymy.Common.ConstKeys;
+using Lanymy.Common.ExtensionFunctions;
+
+namespace Lanymy.Common.Helpers
+{
+
+    public class BinarySerializeHelper
+    {
+        /// <summary>
+        /// 把对象序列化成二进制数据
+        /// </summary>
+        /// <param name="t">要序列化的对象</param>
+        /// <param name="encoding">编码 Null 使用默认编码</param>
+        public static byte[] SerializeToBytes<T>(T t, Encoding encoding = null) where T : class
+        {
+            if (encoding.IfIsNullOrEmpty()) encoding = DefaultSettingKeys.DEFAULT_ENCODING;
+            return encoding.GetBytes(JsonSerializeHelper.SerializeToJson(t));
+        }
+
+        /// <summary>
+        /// 反序列化二进制数据成对象
+        /// </summary>
+        /// <typeparam name="T">要反序列化成对象 的 对象类型</typeparam>
+        /// <param name="bytes">需要反序列化处理的二进制数据</param>
+        /// <param name="encoding">编码 Null 使用默认编码</param>
+        /// <returns></returns>
+        public static T DeserializeFromBytes<T>(byte[] bytes, Encoding encoding = null) where T : class
+        {
+            if (encoding.IfIsNullOrEmpty()) encoding = DefaultSettingKeys.DEFAULT_ENCODING;
+            return JsonSerializeHelper.DeserializeFromJson<T>(encoding.GetString(bytes));
+        }
+
+    }
+
+}
---
+++ src/Commons/Lanymy.Common.Helpers.SerializeHelper.File/FileSerializeHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace Lanymy.Common.Helpers
+{
+
+    public class FileSerializeHelper
+    {
+
+        /// <summary>
+        /// 把对象序列化成二进制文件
+        /// </summary>
+        /// <typeparam name="T">要序列化对象 的 对象类型</typeparam>
+        /// <param name="binaryFileFullPath">要序列化成二进制文件的全路径</param>
+        /// <param name="t">要序列化对象的实例</param>
+        /// <param name="encoding">编码 Null 使用默认编码</param>
+        /// <param name="ifCompressBytes">是否压缩字节数组 默认值 True 压缩形式序列化字节数组</param>
+        /// <returns></returns>
+        public static void SerializeToBytesFile<T>(T t, string binaryFileFullPath, Encoding encoding = null, bool ifCompressBytes = true) where T : class
+        {
+            FileHelper.CreateBinaryFile(binaryFileFullPath, ifCompressBytes ? CompressionHelper.CompressBytesToBytes(BinarySerializeHelper.SerializeToBytes(t, encoding)) : BinarySerializeHelper.SerializeToBytes(t, encoding));
+        }
+
+
+        /// <summary>
+        /// 反序列化二进制文件成对象
+        /// </summary>
+        /// <typeparam name="T">要反序列化成对象的 对象类型</typeparam>
+        /// <param name="binaryFileFullPath">二进制文件全路径</param>
+        /// <param name="encoding">编码 Null 使用默认编码</param>
+        /// <param name="ifDecompressBytes">是否解压缩字节数组 默认值 True 解压缩形式反序列化字节数组</param>
+        /// <returns></returns>
+        public static T DeserializeFromBytesFile<T>(string binaryFileFullPath, Encoding encoding = null, bool ifDecompressBytes = true) where T : class
+        {
+            var bytes = FileHelper.GetBinaryFileBytes(binaryFileFullPath);
+            if (ifDecompressBytes)
+            {
+                bytes = CompressionHelper.DecompressBytesFromBytes(bytes);
+            }
+            return BinarySerializeHelper.DeserializeFromBytes<T>(bytes, encoding);
+        }
+
+    }
+
+}
