b28c64df0f64e7d444f440db909291fb9ee4d970
lanymy <lanyanmiyu@qq.com>
2022-04-21 11:03:59 +0800

    BufferSizeKeys

--- src/Commons/Lanymy.Common.ConstKeys/BufferSizeKeys.cs
+++ src/Commons/Lanymy.Common.ConstKeys/BufferSizeKeys.cs
@@ -15,6 +15,42 @@
         public const int BUFFER_SIZE_4K = BUFFER_SIZE_1K * 4;
 
         /// <summary>
+        /// 缓冲区大小 1024 * 8
+        /// </summary>
+        public const int BUFFER_SIZE_8K = BUFFER_SIZE_1K * 8;
+
+        /// <summary>
+        /// 缓冲区大小 1024 * 16
+        /// </summary>
+        public const int BUFFER_SIZE_16K = BUFFER_SIZE_1K * 16;
+
+        /// <summary>
+        /// 缓冲区大小 1024 * 32
+        /// </summary>
+        public const int BUFFER_SIZE_32K = BUFFER_SIZE_1K * 32;
+
+        /// <summary>
+        /// 缓冲区大小 1024 * 64
+        /// </summary>
+        public const int BUFFER_SIZE_64K = BUFFER_SIZE_1K * 64;
+
+        /// <summary>
+        /// 缓冲区大小 1024 * 128
+        /// </summary>
+        public const int BUFFER_SIZE_128K = BUFFER_SIZE_1K * 125;
+
+        /// <summary>
+        /// 缓冲区大小 1024 * 256
+        /// </summary>
+        public const int BUFFER_SIZE_256K = BUFFER_SIZE_1K * 256;
+
+
+        /// <summary>
+        /// 缓冲区大小 1024 * 512
+        /// </summary>
+        public const int BUFFER_SIZE_512K = BUFFER_SIZE_1K * 512;
+
+        /// <summary>
         /// 缓冲区大小 1024 * 80
         /// </summary>
         public const int BUFFER_SIZE_80K = BUFFER_SIZE_1K * 80;
